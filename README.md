0e812b91133d193258d9e434d4c11f98e345ac4e
Bispo <vitorbrauna22@gmail.com>
2024-02-05 23:14:05 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# US Telephone Number Validator
+
+This is a simple web application that validates US telephone numbers. It's a project for the freeCodeCamp certification.
+
+## Technologies Used
+
+- HTML: Used for structuring the web page.
+- CSS: Used for styling the web page. The CSS includes a custom font from Google Fonts and custom CSS variables for colors.
+- JavaScript: Used for handling user interactions and validating the phone numbers.
+
+## How It Works
+
+The user enters a phone number into the input field and clicks the "Validate" button. The application then checks if the phone number matches the US phone number format. If it does, it displays a message saying the phone number is valid. If it doesn't, it displays a message saying the phone number is invalid. The user can also clear the input field and the result message by clicking the "Clear" button.
+
+## Author
+
+This project was developed by [Vitor Bispo](https://github.com/usasag).
+
+## License
+
+This project is open source and available under the MIT License.
---
+++ script.js
@@ -0,0 +1,29 @@
+const phoneNumberRegex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/;
+const validateButton = document.getElementById('validate-btn');
+const clearButton = document.getElementById('clear-btn');
+const resultMsg = document.getElementById('result-msg');
+
+function validatePhoneNumber() {
+    const phoneNumber = document.getElementById('phone-input').value;
+    const isValid = phoneNumberRegex.test(phoneNumber);
+    
+    if (isValid) {
+        resultMsg.innerHTML = `<p>Valid phone number:</p> \n <p>${phoneNumber}</p>`;
+        resultMsg.style.color = 'green';
+        resultMsg.style.display = 'flex';
+    } else {
+        resultMsg.innerHTML = `<p>Invalid phone number</p> \n <p>${phoneNumber}</p>`;
+        resultMsg.style.color = 'red';
+        resultMsg.style.display = 'flex';
+    }
+}
+
+function clearInput() {
+    document.getElementById('phone-input').value = '';
+    resultMsg.innerHTML = '';
+}
+
+clearButton.addEventListener('click', clearInput);
+validateButton.addEventListener('click', validatePhoneNumber);
+
+// path: index.html
